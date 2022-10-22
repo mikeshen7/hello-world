2dae95715af69ad503ffb800dfbe4ddbbf3e5794
Mike Shen <michael.m.shen@gmail.com>
2022-10-22 21:32:27 +0000

    lab 7 updates

--- app.js
+++ app.js
@@ -1,19 +1,34 @@
-fanCheck();
+let numWins = getData();
 
-function fanCheck(){
+for (let index = 0; index < 2; index++) {
+    if (!validation()){
+        alert("Let's try that again")
+        numWins = getData();
+    }
+}
+
+function getData(){
     alert("To enter this website, you must prove that you are a true Seahawks and Geno Smith fan");
-    let numWins = Number(prompt("How many wins will the Seahawks have this year?"));
+    let data = Number(prompt("How many wins will the Seahawks have this year?"));
+    return data;
+}
 
+function validation(){
     if (isNaN(numWins)) { // checks to see if it's a number
         alert("Invalid entry.");
+        return false
     } else if ((numWins > 17)) { // checks for unrealistic number
         document.write(numWins  + "??? This is American football we are talking about... There's only 17 games.");
+        return true
     } else if (numWins > 10) { // Lots of wins - fan
         document.write(numWins + "! I like how you think");
+        return true
     } else if (numWins < 5) { // not many wins - not a fan
         document.write(numWins + "? Are you sure you're on the right website?  This is a Seahawk / Geno Smith FAN website");
+        return true
     } else { // intermediate level of wins
         document.write(numWins + "... You may enter, fair weather fan.");
+        return true
     }
 }
 
